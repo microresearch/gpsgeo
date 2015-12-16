e96134b2f59f4dec1d4aa02ecd223f31bc0dabd5
root <root@xxxxx>
2015-12-17 00:40:01 +0100

    xxx

--- gpsgeomancy003.py
+++ gpsgeomancy003.py
@@ -24,8 +24,10 @@ import logging
 #from ib.opt import ibConnection, message
 #import mysql.connector
 
-
 company_dict={'Fire': ['ACN', 'ACE', 'ATVI', 'ADBE', 'AFL', 'AMG', 'AKAM', 'ADS', 'ALL', 'GOOGL', 'GOOG', 'ALTR', 'AXP', 'AIG', 'AMT', 'AMP', 'ADI', 'AON', 'AIV', 'AAPL', 'AMAT', 'AIZ', 'ADSK', 'ADP', 'AVGO', 'AVB', 'BAC', 'BK', 'BBT', 'BRK-B', 'BLK', 'HRB', 'BXP', 'BRCM', 'CA', 'COF', 'CBG', 'SCHW', 'CB', 'CINF', 'CSCO', 'C', 'CTXS', 'CME', 'CTSH', 'CMA', 'CSC', 'CCI', 'DFS', 'ETFC', 'EBAY', 'EA', 'EMC', 'EFX', 'EQIX', 'EQR', 'ESS', 'FFIV', 'FB', 'FIS', 'FITB', 'FSLR', 'FISV', 'BEN', 'GGP', 'GNW', 'GS', 'HRS', 'HIG', 'HCP', 'HPQ', 'HST', 'HCBK', 'HBAN', 'INTC', 'ICE', 'IBM', 'INTU', 'IVZ', 'JPM', 'JNPR', 'KEY', 'KIM', 'KLAC', 'LRCX', 'LM', 'LUK', 'LNC', 'LLTC', 'L', 'MTB', 'MAC', 'MMC', 'MA', 'MHFI', 'MET', 'MCHP', 'MU', 'MSFT', 'MCO', 'MS', 'MSI', 'NDAQ', 'NAVI', 'NTAP', 'NFLX', 'NTRS', 'NVDA', 'ORCL', 'PAYX', 'PYPL', 'PBCT', 'PCL', 'PNC', 'PFG', 'PGR', 'PLD', 'PRU', 'PSA', 'QRVO', 'QCOM', 'O', 'RHT', 'RF', 'CRM', 'SNDK', 'STX', 'SPG', 'SWKS', 'SLG', 'STT', 'STI', 'SYMC', 'TROW', 'TEL', 'TDC', 'TXN', 'TRV', 'TMK', 'TSS', 'USB', 'UNM', 'VTR', 'VRSN', 'V', 'VNO', 'WFC', 'HCN', 'WDC', 'WU', 'WY', 'XRX', 'XLNX', 'XL', 'YHOO', 'ZION'], 'Earth': ['MMM', 'ADT', 'AES', 'GAS', 'APD', 'ARG', 'AA', 'ALLE', 'AEE', 'AAL', 'AEP', 'AME', 'APH', 'APC', 'APA', 'AVY', 'BHI', 'BLL', 'BA', 'CHRW', 'COG', 'CAM', 'CAT', 'CNP', 'CF', 'CHK', 'CVX', 'XEC', 'CTAS', 'CMS', 'CPGX', 'COP', 'CNX', 'ED', 'GLW', 'CSX', 'CMI', 'DHR', 'DE', 'DAL', 'DVN', 'DO', 'D', 'DOV', 'DOW', 'DTE', 'DD', 'DUK', 'DNB', 'EMN', 'ETN', 'ECL', 'EIX', 'EMR', 'ESV', 'ETR', 'EOG', 'EQT', 'ES', 'EXC', 'EXPD', 'XOM', 'FAST', 'FDX', 'FE', 'FLIR', 'FLS', 'FLR', 'FMC', 'FTI', 'FCX', 'GD', 'GE', 'GWW', 'HAL', 'HP', 'HES', 'HON', 'ITW', 'IR', 'IP', 'IFF', 'IRM', 'JEC', 'JBHT', 'KSU', 'KMI', 'LLL', 'LEG', 'LMT', 'LYB', 'MRO', 'MPC', 'MLM', 'MAS', 'WRK', 'MON', 'MOS', 'MUR', 'NOV', 'NFX', 'NEM', 'NEE', 'NLSN', 'NI', 'NBL', 'NSC', 'NOC', 'NRG', 'NUE', 'OXY', 'OKE', 'OI', 'PCAR', 'PH', 'PNR', 'POM', 'PCG', 'PSX', 'PNW', 'PXD', 'PBI', 'PPG', 'PPL', 'PX', 'PCP', 'PEG', 'PWR', 'RRC', 'RTN', 'RSG', 'RHI', 'ROK', 'COL', 'ROP', 'R', 'SCG', 'SLB', 'SEE', 'SRE', 'SHW', 'SIAL', 'SO', 'LUV', 'SWN', 'SE', 'SRCL', 'TE', 'TSO', 'TXT', 'RIG', 'TYC', 'UNP', 'UAL', 'UPS', 'URI', 'UTX', 'VLO', 'VRSK', 'VMC', 'WM', 'WMB', 'WEC', 'XEL', 'XYL'], 'Water': ['ABT', 'ABBV', 'AAP', 'AET', 'A', 'AGN', 'ALXN', 'MO', 'AMZN', 'ABC', 'AMGN', 'ADM', 'AN', 'AZO', 'BCR', 'BXLT', 'BAX', 'BDX', 'BBBY', 'BBY', 'BIIB', 'BWA', 'BSX', 'BMY', 'BF-B', 'CVC', 'CPB', 'CAH', 'HSIC', 'KMX', 'CCL', 'CBS', 'CELG', 'CERN', 'CMG', 'CI', 'CLX', 'COH', 'KO', 'CCE', 'CL', 'CMCSA', 'CMCSK', 'CAG', 'STZ', 'COST', 'CVS', 'DHI', 'DRI', 'DVA', 'DLPH', 'XRAY', 'DISCA', 'DISCK', 'DG', 'DLTR', 'DPS', 'EW', 'ENDP', 'EL', 'EXPE', 'ESRX', 'F', 'FOSL', 'GME', 'GPS', 'GRMN', 'GIS', 'GM', 'GPC', 'GILD', 'GT', 'HBI', 'HOG', 'HAR', 'HAS', 'HCA', 'HD', 'HRL', 'HUM', 'IPG', 'ISRG', 'JNJ', 'JCI', 'K', 'GMCR', 'KMB', 'KSS', 'KHC', 'KR', 'LB', 'LH', 'LEN', 'LLY', 'LOW', 'M', 'MNK', 'MAR', 'MAT', 'MKC', 'MCD', 'MCK', 'MJN', 'MDT', 'MRK', 'KORS', 'MHK', 'TAP', 'MDLZ', 'MNST', 'MYL', 'NWL', 'NWSA', 'NWS', 'NKE', 'JWN', 'ORLY', 'OMC', 'PDCO', 'PEP', 'PKI', 'PRGO', 'PFE', 'PM', 'RL', 'PCLN', 'PG', 'PHM', 'PVH', 'DGX', 'REGN', 'RAI', 'ROST', 'RCL', 'SNI', 'SIG', 'SJM', 'SNA', 'STJ', 'SWK', 'SPLS', 'SBUX', 'HOT', 'SYK', 'SYY', 'TGT', 'TGNA', 'THC', 'HSY', 'TMO', 'TIF', 'TWX', 'TWC', 'TJX', 'TSCO', 'TRIP', 'FOXA', 'FOX', 'TSN', 'UA', 'UNH', 'UHS', 'URBN', 'VFC', 'VAR', 'VRTX', 'VIAB', 'WMT', 'WBA', 'DIS', 'WAT', 'ANTM', 'WHR', 'WFM', 'WYN', 'WYNN', 'YUM', 'ZBH', 'ZTS'], 'Air': ['T', 'CTL', 'FTR', 'LVLT', 'VZ']}
+
+
+# TODO: test and tweak buy and sell *eg. ticker*, updating and accounting
 
 
 def loadTLE(path):
@@ -355,7 +357,7 @@ portfolio.setdefault("Fire",{})
 portfolio.setdefault("Air",{})
 portfolio.setdefault("Water",{})
 
-def exec_trade(self, trade_json):
+def exec_trade(trade_json):
     server = socket.create_connection(self.trade_serv)
     server.settimeout(10)  # 10 seconds
     msg = trade_json.encode('utf-8')
@@ -368,10 +370,10 @@ def exec_trade(self, trade_json):
     server.close()
 
 
-def exec_buy(self, shares, price):
+def exec_buy(whichone, shares, price):
     # actually buy shares on the market.
     trade = {"pass_client": pass_key,
-             "contract": {"m_symbol": ticker,
+             "contract": {"m_symbol": whichone,
                           "m_secType": "STK",
                           "m_exchange": "SMART",
                           "m_currency": "USD"
@@ -379,7 +381,7 @@ def exec_buy(self, shares, price):
                  "order": {"m_action": "BUY",
                            "m_totalQuantity": shares,
                            "m_orderType": "LMT",
-                           "m_lmtPrice": float(price),
+                           "m_lmtPrice": float(price), # what is price?
                            "m_tif": "DAY",
                            "m_goodAfterTime": "",
                            "m_goodTillDate": ""
@@ -389,10 +391,10 @@ def exec_buy(self, shares, price):
     logging.debug("buying", shares, "shares at", price)
     exec_trade(trade_json)
 
-def exec_sell(self, shares, price):
+def exec_sell(whichone,shares, price):
     # actually sell on the market
     trade = {"pass_client": pass_key,
-             "contract": {"m_symbol": ticker,
+             "contract": {"m_symbol": whichone,
                           "m_secType": "STK",
                           "m_exchange": "SMART",
                           "m_currency": "USD"
@@ -446,6 +448,8 @@ def whattodo(figure):
 
 # do buy action
 
+#exec_buy(whichone, 1, bought.get_price()):
+
     elif action=="sell":
         #choose a random from elemental dict
         if portfolio[elemental]:
@@ -460,7 +464,9 @@ def whattodo(figure):
             else:
                 print "No action taken"
 
-# do sell section
+# do sell action
+
+#exec_sell(whichone, 1, sold.get_price()):
 
 #    print portfolio    
 
@@ -486,10 +492,7 @@ def main():
     nyse.lat = np.deg2rad(40.714353)
     nyse.long = np.deg2rad(-74.005973)
 
-
-    #consult at specific asuspicious time which is opening!: and pull this out into function...
-
-    # check that market is open!
+    #consult at specific auspicious time which is opening of NYSE!
 
     while True:
     # wait for stock exchange to open 
@@ -531,8 +534,9 @@ def main():
 
             # buy or sell
             whattodo(chosen)
+
+            # update and print account details
+
         
-
-
 if __name__ == '__main__':
     sys.exit(main())
